15e87affda3c7cf208cf1078c6a726ac923960c1
SarahLeCorre <72316644+SarahLeCorre@users.noreply.github.com>
2021-10-13 17:06:38 +0200

    Update opérateurs.c

--- ProgC_TP1/opérateurs.c
+++ ProgC_TP1/opérateurs.c
@@ -1,3 +1,9 @@
+/* 
+*TP1, exercice 1.5
+*Auteurs : Sarah LE CORRE, Amandine PICCINALI
+*/
+
+
 #include <stdio.h>
 
 /*Ecrivez un programme qui utilise deux variables et testez les différents opérateurs arithmétiques et logiques */
@@ -6,6 +12,8 @@
 int main () {
 	int a = 16;
 	int b =3;
+	
+	// Affichage des resultats
 	
 	printf("a<b = %d \n",a<b);
 	printf("a>b = %d \n",a>b);
@@ -19,5 +27,6 @@ int main () {
 	printf("b=++a => b=%d et a=%d \n", b=++a, a);		// reprend les valeurs de a et de b
 	printf("b=a-- => b=%d et a=%d \n", b=a--, a);		// qui ont été modifiées pas les 
 	printf("b=--a => b=%d et a=%d \n", b=--a, a);		//originales
+	
 	return 0;
 }
